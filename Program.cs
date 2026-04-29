736a8d5ae4124aa069a88474d30c2fb428ba2276
ASUS <ASUS@DESKTOP-SAPTPIC>
2026-04-29 16:53:48 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RectangleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа работает");
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace RectangleApp
+{
+    public class Rectangle
+    {
+        private double width;
+        private double height;
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value > 0)
+                    height = value;
+                else
+                    height = 1;
+            }
+        }
+
+        public void Init(double w, double h)
+        {
+            if (w > 0 && h > 0)
+            {
+                width = w;
+                height = h;
+            }
+            else
+            {
+                width = 2;
+                height = 1;
+            }
+        }
+
+        public double Area()
+        {
+            return width * height;
+        }
+
+        public static Rectangle Add(Rectangle r1, Rectangle r2)
+        {
+            Rectangle result = new Rectangle();
+            result.Init(r1.width + r2.width, r1.height + r2.height);
+            return result;
+        }
+    }
+}
